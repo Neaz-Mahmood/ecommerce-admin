464686c2d1f4d0d4c19c66e2029406ceebe08443
Neaz Mahmood <neazn694@gmail.com>
2023-10-26 14:27:29 +0600

    Added store patch and delete method

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -10,6 +10,15 @@ import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
+import { 
+    Form, 
+    FormControl, 
+    FormField, 
+    FormItem, 
+    FormLabel, 
+    FormMessage
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 interface SettingsFormProps {
     initialData: Store;
@@ -33,6 +42,10 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
         defaultValues: initialData
     });
 
+    const onSubmit = async (data: SettingsFormValues) => {
+        console.log(data);        
+    };
+
     return (
         <>
             <div className="flex items-center justify-between">
@@ -49,6 +62,28 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
                 </Button>
             </div>
             <Separator />
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+                    <div className="grid grid-cols-3 gap-8">
+                        <FormField 
+                            control={form.control}
+                            name="name"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Name</FormLabel>
+                                    <FormControl>
+                                        <Input disabled={loading} placeholder="Store name" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+                    <Button disabled={loading} className="ml-auto" type="submit" >
+                        Save Changes
+                    </Button>
+                </form>
+            </Form>
         </>
     );
 };
